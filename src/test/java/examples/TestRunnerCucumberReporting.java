dd1fdb067b2237e1db1c19365d19d606a6e7907b
Cyril <cyril.george@katzion.com>
2021-10-08 21:47:04 +0530

    tear down method added

--- src/test/java/examples/TestRunnerCucumberReporting.java
+++ src/test/java/examples/TestRunnerCucumberReporting.java
@@ -2,7 +2,9 @@ package examples;
 
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
+import java.awt.*;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -10,6 +12,7 @@ import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportBuilder;
 import org.apache.commons.io.FileUtils;
 import static org.junit.Assert.*;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 
 public class TestRunnerCucumberReporting {
@@ -29,5 +32,15 @@ public class TestRunnerCucumberReporting {
         Configuration config = new Configuration(new File("target"), "swaglabs");
         ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
         reportBuilder.generateReports();
+    }
+    @AfterAll
+    public static void tearDown() {
+        try {
+            Desktop.getDesktop()
+                    .browse(new File("target/cucumber-html-reports/report-feature_1407342867.html").toURI());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- src/test/java/examples/TestRunnerParallel.java
+++ src/test/java/examples/TestRunnerParallel.java
@@ -3,7 +3,11 @@ package examples;
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 class TestRunnerParallel {
 
@@ -13,5 +17,15 @@ class TestRunnerParallel {
                 //.outputCucumberJson(true)
                 .parallel(4);
         assertEquals(0, results.getFailCount(), results.getErrorMessages());
+    }
+    @AfterAll
+    public static void tearDown() {
+        try {
+            Desktop.getDesktop()
+                    .browse(new File("target/karate-reports/karate-summary.html").toURI());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
